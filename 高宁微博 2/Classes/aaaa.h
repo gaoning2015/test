d6aed83b43044327f1b00b182d503f42f88827d8
gaoning <289706335@163.com>
2015-05-22 09:40:12 +0800

    更新的

---
+++ 高宁微博 2/Classes/aaaa.h
@@ -0,0 +1,13 @@
+//
+//  aaaa.h
+//  高宁微博
+//
+//  Created by gaoning on 15/5/22.
+//  Copyright (c) 2015年 GN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface aaaa : UITabBarController
+
+@end
---
+++ 高宁微博 2/Classes/aaaa.m
@@ -0,0 +1,13 @@
+//
+//  aaaa.m
+//  高宁微博
+//
+//  Created by gaoning on 15/5/22.
+//  Copyright (c) 2015年 GN. All rights reserved.
+//
+
+#import "aaaa.h"
+
+@implementation aaaa
+
+@end
